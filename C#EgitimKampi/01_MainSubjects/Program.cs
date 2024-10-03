62b8a2225dc34520f0441223b74216bc55aeb645
Süleyman Ay <suleymanay.it@gmail.com>
2024-10-03 18:21:44 +0200

    C# Eğitim Kampı
    C# Eğitim Kampı Ders 2 - String, Int, Double, Char Değişkenler, Klavye Veri Girişleri, Dönüşümler

---
+++ C#EgitimKampi/01_MainSubjects/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_MainSubjects
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Yazdırma Komutları
+
+            //Console.WriteLine("Merhaba Dünya");
+            //Console.Write("Selam");
+
+            //Console.WriteLine("***** Yemek Kategorileri ****");
+            //Console.WriteLine();
+            //Console.WriteLine("1-Çorbalar");
+            //Console.WriteLine("2-Ana Yemekler");
+            //Console.WriteLine("3-Soğuk Başlangıçlar");
+            //Console.WriteLine("4-Salatalar");
+            //Console.WriteLine("5-Tatlılar");
+            //Console.WriteLine("6-İçecekler");
+            //Console.WriteLine();
+            //Console.WriteLine("***** Yemek Kategorileri *****");
+
+            #endregion
+
+            #region String Değişkenler
+
+            //string customerName;
+            //string customerSurname;
+            //string customerPhone;
+            //string customerEmail, district, city;
+
+            //customerName = "Ali";
+            //customerSurname = "Çınar";
+            //customerPhone = "+90 500 400 30 20";
+            //customerEmail = "deneme@gmail.com";
+            //district = "Kadıköy";
+            //city = "İstanbul";
+
+            //Console.WriteLine("**** Rezervasyon Kartı ****");
+            //Console.WriteLine();
+            //Console.WriteLine("------------------------------------------");
+            //Console.WriteLine("Müşteri: " + customerName + " " + customerSurname);
+            //Console.WriteLine("İletişim: " + customerPhone);
+            //Console.WriteLine("Email: " + customerEmail);
+            //Console.WriteLine("Adres: " + district + "/" + city);
+            //Console.WriteLine("------------------------------------------");
+
+
+
+            #endregion
+
+            #region Int Değişkenler
+
+            int hamburgerPrice = 300;
+            int cokePrice = 35;
+            int waterPrice = 10;
+            int friesPrice = 50;
+            int pizzaPrice = 250;
+            int lemonadePrice = 30;
+
+            Console.WriteLine("***** Restoran Menü Fiyatları *****");
+            Console.WriteLine();
+            Console.WriteLine("------ Hamburger: " + hamburgerPrice + " TL");
+            Console.WriteLine("------ Kola: " + cokePrice + " TL");
+            Console.WriteLine("------ Su: " + waterPrice + " TL");
+            Console.WriteLine("------ Limonata: " + lemonadePrice + " TL");
+            Console.WriteLine("------ Kızartma Tabağı: " + friesPrice + " TL");
+            Console.WriteLine("------ Pizza: " + pizzaPrice + " TL");
+            Console.WriteLine();
+            Console.WriteLine("***** Restoran Menü Fiyatları *****");
+            Console.WriteLine();
+
+            Console.WriteLine();
+            int hamburgerCount;
+            int cokeCount;
+            int waterCount;
+            int friesCount;
+            int pizzaCount;
+            int lemonadeCount;
+
+            int totalPrice = 0;
+
+
+
+
+            #endregion
+
+            Console.Read();
+        }
+    }
+}
